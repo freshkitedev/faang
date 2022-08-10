48debe485a1ab2cdf971f1d915fd1551efa3049b
Abinaya-A <abibaby251@gmail.com>
2022-08-10 16:45:07 +0530

    updated question with test case

--- basic/if_condition/question/1_positive_or_negative.py
+++ basic/if_condition/question/1_positive_or_negative.py
@@ -1,2 +1,27 @@
-# Write a program to find a given number is positive negative or zero.
+# Write a program to find a given number is positive negative or zero.#If the number is positive you should print POSITIVE , or if the number 
+# is negative you should print NEGATIVE or if the number is 0 you 
+# should print ZERO.
+#Complete the function below and store your result in ans variable
+def positive_or_negative(num):
+   #Practise yourself : write your code here 
+   ans = ""
+   return ans
+import unittest
 
+class Test(unittest.TestCase):
+    def test_positive_or_negative(self):
+        num = 5
+        actual = positive_or_negative(num)
+        expected = "POSITIVE"
+        self.assertEqual(actual, expected)
+    def test_positive_or_negative(self):
+        num = -2
+        actual = positive_or_negative(num)
+        expected = "NEGATIVE"
+        self.assertEqual(actual, expected)
+    def test_positive_or_negative(self):
+        num = 0
+        actual = positive_or_negative(num)
+        expected = "ZERO"
+        self.assertEqual(actual, expected)
+unittest.main(verbosity=2)
