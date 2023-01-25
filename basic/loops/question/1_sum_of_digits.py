a54ed4210b3d1e287cc11ab4ed162e8ecaf66e66
Dhakshanapandi <dhakshanapandi84@gmail.com>
2023-01-25 12:01:26 +0530

    Solved sumofdigits

--- basic/loops/question/1_sum_of_digits.py
+++ basic/loops/question/1_sum_of_digits.py
@@ -1,2 +1,10 @@
 # write a program to find sum of digits of an integer.
+def sumofint(n):
+    sum = 0
+    while(n>0):
+        rem = n % 10
+        sum = sum + rem
+        n = n//10
+    return sum    
 
+print(sumofint(154))
