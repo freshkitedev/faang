14a9aa51fc66890955d47463d487be5bb665d6a8
monika2710 <monika2710moni@gmail.com>
2023-01-28 11:13:50 +0530

    ready

--- basic/loops/question/8_swap_first_last_digit.py
+++ basic/loops/question/8_swap_first_last_digit.py
@@ -1,9 +1,25 @@
 # Write a program to swap first and last digit in a given number
-
+def digit_count(n):
+    count = 0
+    while (n):
+        count += 1
+        n //= 10
+    return count
+        
 def swapFirstLast(n):
     #code here
-    a = 0
-    return a
+    c = digit_count(n)
+    if c == 1:
+        return n
+    ld = n % 10
+    p = 10**(c-1)
+    fd = n // p
+    n = n % p
+    n = n // 10
+    n = n*10 + fd
+    ld*= p
+    n = ld + n
+    return n
 
 import unittest
 
