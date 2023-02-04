b90e58425dc269e725bb205532402eb8f56529a3
Dhakshanapandi <dhakshanapandi84@gmail.com>
2023-02-04 16:19:21 +0530

    max num in list

---
+++ basic/loops/question/14_max_num_in_list.py
@@ -0,0 +1,41 @@
+# Write Program to find maximum number in list
+def max_num_in_list(arr):
+   
+    return max              
+
+import unittest
+
+class Test(unittest.TestCase):
+    def test_search1(self):
+        actual = max_num_in_list([3,42,34,5,2])
+        expected = 42
+        self.assertEqual(actual, expected)
+    def test_search2(self):
+        actual = max_num_in_list([34,9,3,4,-1,-34])
+        expected = 34
+        self.assertEqual(actual, expected)
+    def test_search3(self):
+        actual = max_num_in_list([34,9,3,4,-1,-34,98])
+        expected = 98
+        self.assertEqual(actual, expected)
+    def test_search4(self):
+        actual = max_num_in_list([1])
+        expected = 1
+        self.assertEqual(actual, expected)
+    def test_search5(self):
+        actual = max_num_in_list([-2,-1,0])
+        expected = 0
+        self.assertEqual(actual, expected)
+unittest.main(verbosity=2)
+
+
+
+
+
+
+
+
+
+
+
+
